0c2f92e18cb92de2413c22b164559eeba28ebd37
Stiven-38 <129075107+Stiven-38@users.noreply.github.com>
2023-05-17 18:53:33 -0500

    enlace

---
+++ ejercicio tuplas - copia.py
@@ -0,0 +1,16 @@
+listaAnimales = ("perro", "gatos", "tortugas", "conejos")
+
+print(listaAnimales[1])
+
+
+print("\n")
+
+tipoColores =("naranaja","rojo","verde","amarillo")
+print(tipoColores[3])
+
+print("\n")
+
+tipoNombres=("melissa","kamila","vanessa","paula","stiven")
+print(tipoNombres[4])
+
+
